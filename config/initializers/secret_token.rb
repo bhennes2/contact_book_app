2c44f24530700e706a359efbe43f1b4af0503e71
Brendan Hennessy <bhennes2@gmail.com>
2011-02-03 14:08:10 -0600

    'Initial'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ContactBookApp::Application.config.secret_token = '1aa5485cfb5bf3ba737af201ef794b4722188402f36be3ff148e5307d9fcb2e582df2b16bd8b53df778cd497a836e09850bd24b82147fed696e39e1b190b99a7'
